2c202cf5b2c037e7d92033502275300334eb0eb9
otaviootavio <otaviovacari@usp.br>
2023-11-28 00:04:28 -0300

    implement select borrower

--- packages/nextjs/components/sassy/AddExpenseToRoom.tsx
+++ packages/nextjs/components/sassy/AddExpenseToRoom.tsx
@@ -1,40 +1,59 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { AddressInput, EtherInput } from "../scaffold-eth";
+import { EtherInput } from "../scaffold-eth";
+import SelectWhoHasBorrowed from "./SelectWhoHasBorrowed";
 import { parseEther } from "viem";
-import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+
+type GetRoomDetailsResponse = {
+  owner: string;
+  isOpen: boolean;
+  participantList: string[];
+};
 
 const AddExpenseToRoom = () => {
   const [ammount, setAmmount] = useState<string>("0");
-  const [payer, setPayer] = useState<string>("0");
-  const [borrower, setBorrower] = useState<string>("0");
-
+  const [borrowers, setBorrowers] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [roomDetailsResponse, setRoomDetailsResponse] = useState<GetRoomDetailsResponse | null>(null);
   const router = useRouter();
   const room_id: string = router.query.id ? router.query.id[0] : "0";
 
   const { writeAsync } = useScaffoldContractWrite({
     contractName: "SharedExpenses",
     functionName: "addExpense",
-    args: [BigInt(room_id), parseEther(ammount), [borrower]],
+    args: [BigInt(room_id), parseEther(ammount), borrowers],
+  });
+
+  useScaffoldContractRead({
+    contractName: "SharedExpenses",
+    functionName: "getRoomDetails",
+    args: [BigInt(room_id)],
+    onSuccess: async (data: any) => {
+      const roomDetailsResponse_temp = parseToGetRoomDetailsResponse(data);
+      setRoomDetailsResponse(roomDetailsResponse_temp);
+      setIsLoading(false);
+    },
   });
 
   return (
     <div className="card w-full md:w-1/2 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">AddExpenseToRoom</h2>
+        <h2 className="card-title">Add people that owe you!</h2>
+        <label htmlFor="borrower">
+          Who has borrowed?
+          <br />
+          {isLoading || (
+            <SelectWhoHasBorrowed
+              selectedValues={borrowers}
+              setSelectedValues={setBorrowers}
+              options={roomDetailsResponse ? roomDetailsResponse?.participantList : [""]}
+            />
+          )}
+        </label>
         <label htmlFor="ammount">
           Amout
           <EtherInput onChange={setAmmount} value={ammount} />
-        </label>
-        <br />
-        <label htmlFor="payer">
-          Who is paying?
-          <AddressInput onChange={setPayer} value={payer} placeholder="Input your address" />
-        </label>
-        <br />
-        <label htmlFor="borrower">
-          Who has borrowed?
-          <AddressInput onChange={setBorrower} value={borrower} placeholder="Input your address" />
         </label>
         <button
           className="btn btn-sm btn-primary"
@@ -48,5 +67,32 @@ const AddExpenseToRoom = () => {
     </div>
   );
 };
+
+function parseToGetRoomDetailsResponse(input: any): GetRoomDetailsResponse | null {
+  try {
+    if (typeof input !== "object" || input === null) {
+      throw new Error("Input is not an object.");
+    }
+
+    const [owner, isOpen, participantList] = input;
+
+    if (typeof owner !== "string") {
+      throw new Error("owner is not a string.");
+    }
+
+    if (typeof isOpen !== "boolean") {
+      throw new Error("isOpen is not a boolean.");
+    }
+
+    if (!Array.isArray(participantList) || !participantList.every(item => typeof item === "string")) {
+      throw new Error("participantList is not an array of strings.");
+    }
+
+    return { owner, isOpen, participantList };
+  } catch (error) {
+    console.error("Error parsing GetRoomDetailsResponse:", error);
+    return null;
+  }
+}
 
 export default AddExpenseToRoom;
